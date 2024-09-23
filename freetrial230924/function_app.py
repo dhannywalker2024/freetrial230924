4beb887d0f6dce6b42ce4c6ff7a18f544b501477
dhannywalker2024 <dhanny86walker@gmail.com>
2024-09-24 03:07:41 +0530

    Add files via upload

---
+++ freetrial230924/function_app.py
@@ -0,0 +1,30 @@
+import logging
+import azure.functions as func
+import json
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # Get the 'name' and 'pronoun' from the query string or the request body
+    name = req.params.get('name')
+    pronoun = req.params.get('pronoun')
+
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+            pronoun = req_body.get('pronoun')
+
+    if name and pronoun:
+        return func.HttpResponse(
+            f"Hello {name}, nice to know that your pronoun is {pronoun}.",
+            status_code=200
+        )
+    else:
+        return func.HttpResponse(
+            "Please provide both a name and a pronoun.",
+            status_code=400
+        )
